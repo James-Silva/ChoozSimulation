11b43c6ed293930e82301cc504def6b32c41325d
vsibille <vsibille@mit.edu>
2017-02-27 14:41:47 -0500

    Spacing

--- include/PrimaryGeneratorAction.hh
+++ include/PrimaryGeneratorAction.hh
@@ -28,50 +28,50 @@ class DetectorConstruction;
 class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
 {
 public:
-	PrimaryGeneratorAction(DetectorConstruction*, HistoManager*);
-	virtual ~PrimaryGeneratorAction() = default;
+    PrimaryGeneratorAction(DetectorConstruction*, HistoManager*);
+    virtual ~PrimaryGeneratorAction() = default;
 
-	virtual void GeneratePrimaries(G4Event*);
-	void buildSource(const G4String& particleName, G4double energy);
-	void setGammaPosition();
-	void setNeutronPosition();
-	void setNeutronMomentum();
-	void setNewNeutronSource(G4double energy);
+    virtual void GeneratePrimaries(G4Event*);
+    void buildSource(const G4String& particleName, G4double energy);
+    void setGammaPosition();
+    void setNeutronPosition();
+    void setNeutronMomentum();
+    void setNewNeutronSource(G4double energy);
     void setNewGammaSource(G4double energy);
-	void setSourceRadius(G4double radius);
-	void setSourceHeight(G4double radius);
-	void setSourceHeightOffset(G4double offset);
-	void SetSpectralData(const std::string& fileName);
-	void setNewNeutronSpectrumSource();
-	void setNewNeutronSpectrumSource_LogX();
-	void setPointNeutronPosition();
-	void setPointGammaPosition();
-	void SetNeutronPointSource(G4ThreeVector pos);
-	void SetGammaPointSource(G4ThreeVector pos);
-	void SetGenerator(G4String generatorType);
-	void setSourceThickness(G4double thickness);
-	G4ThreeVector GenerateSideWallEvent(G4double radius,G4double height,G4double offset);
+    void setSourceRadius(G4double radius);
+    void setSourceHeight(G4double radius);
+    void setSourceHeightOffset(G4double offset);
+    void SetSpectralData(const std::string& fileName);
+    void setNewNeutronSpectrumSource();
+    void setNewNeutronSpectrumSource_LogX();
+    void setPointNeutronPosition();
+    void setPointGammaPosition();
+    void SetNeutronPointSource(G4ThreeVector pos);
+    void SetGammaPointSource(G4ThreeVector pos);
+    void SetGenerator(G4String generatorType);
+    void setSourceThickness(G4double thickness);
+    G4ThreeVector GenerateSideWallEvent(G4double radius,G4double height,G4double offset);
     G4ThreeVector GenerateTopEvent(G4double radius,G4double height);
     G4ThreeVector GenerateIsotropicVector();
 private:
-    TH1D 							h_Spectrum;
-    std::vector<double> 			vec_SpectralEnergies;
-	std::vector<double> 			vec_SpectralVals;
-	G4double                        neutronradius;
-	G4double                        sourceoffsetz;
-	G4double                        sourceRadius;
-	G4double                        sourceHeight;
-	G4double 			bottomProbability; //ratio of the bottom surface to the side surface
-	G4double                        sourcethickness;
-	std::unique_ptr<PrimaryGeneratorMessenger>		messenger;
-	std::unique_ptr<G4ParticleGun>          		particleGun;
-	DetectorConstruction*    				detectorConstruction;
-	HistoManager*						fHistoManager;
-	G4ThreeVector                   neutronsourcepos;
-	G4ThreeVector                   gammasourcepos;
-	G4VUserPrimaryGeneratorAction*  particleSource;    //pointer a to G4  class
+    TH1D 			h_Spectrum;
+    std::vector<double> 	vec_SpectralEnergies;
+    std::vector<double> 	vec_SpectralVals;
+    G4double                    neutronradius;
+    G4double                    sourceoffsetz;
+    G4double                    sourceRadius;
+    G4double                    sourceHeight;
+    G4double 			bottomProbability; //ratio of the bottom surface to the side surface
+    G4double                    sourcethickness;
+    std::unique_ptr<PrimaryGeneratorMessenger>		messenger;
+    std::unique_ptr<G4ParticleGun>          		particleGun;
+    DetectorConstruction*    				detectorConstruction;
+    HistoManager*						fHistoManager;
+    G4ThreeVector                   neutronsourcepos;
+    G4ThreeVector                   gammasourcepos;
+    G4VUserPrimaryGeneratorAction*  particleSource;    //pointer a to G4  class
 
-	G4String sourceType; 
+    G4String sourceType; 
     bool logaxis; 
     
     void updateBottomProbability();
